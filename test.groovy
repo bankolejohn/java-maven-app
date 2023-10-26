b94ca5199a78b1a8b9f6c38ccc7aae40a5f7b122
Bankole John <bankolejohn@gmail.com>
2023-10-26 10:23:06 +0000

    update groovy script

--- test.groovy
+++ test.groovy
@@ -4,10 +4,12 @@ def buildJar() {
 
 def buildAppImage() {
     echo 'building the app image ...'
-    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASSWORD', usernameVariable: 'USER')])
-    sh 'docker build -t newimage:2.0 .'
-    sh "echo $PASSWORD | docker login -u $USER --password-stdin"
-    sh 'docker push bankolejohn/newimage:2.0'
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
+        sh 'docker build -t newimage:2.0 .'
+        sh "echo $PASSWORD | docker login -u $USER --password-stdin"
+        sh 'docker push bankolejohn/newimage:2.0'
+    }
+    
 
 }
 
